3d0ff76c6bfce253fcf9152555ece914a2d4c17d
caiquepirs <c.pires@ba.estudante.senai.br>
2025-03-27 04:12:19 -0300

    Criando um ArrayList para armazenar todos os clientes cadastrados no sistema da Biblioteca

--- src/main/java/module/biblioteca/model/Biblioteca.java
+++ src/main/java/module/biblioteca/model/Biblioteca.java
@@ -1,14 +1,15 @@
-package modules.biblioteca;
+package module.biblioteca.model;
 
-import modules.livro.Autor;
-import modules.livro.Livro;
+import module.cliente.model.Cliente;
+import module.livro.model.Autor;
+import module.livro.model.Livro;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Biblioteca {
-
     List<Livro> livros = new ArrayList<>();
     List<Autor> autores = new ArrayList<>();
+    List<Cliente> clientes = new ArrayList<>();
     List<Emprestimo> emprestimos = new ArrayList<>();
 }
