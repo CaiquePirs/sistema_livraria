49b40e2c384184321ed236d5eb9887f604fbd616
caiquepirs <c.pires@ba.estudante.senai.br>
2025-03-30 10:56:53 -0300

    Atualizando atributos da classe livro, chamando método para converter a data de Cadastro do livro e criando o método para exibir os dados do livro

--- src/main/java/module/livro/model/Livro.java
+++ src/main/java/module/livro/model/Livro.java
@@ -1,78 +1,77 @@
 package module.livro.model;
 
+import module.biblioteca.service.FormatarData;
+
+import java.sql.Date;
 import java.time.LocalDate;
 
 public class Livro {
 
     private int id;
     private String titulo;
-    private Autor autor;
+    private String autor;
     private boolean status = false;
-    private LocalDate dataCadastro;
-    private  LocalDate dataAtualizacao;
+    private Date dataCadastro;
+    private Date dataAtualizacao;
 
-    Livro(int id, String titulo, Autor autor, LocalDate dataCadastro){
+   public Livro(int id, String titulo, String autor, LocalDate data){
         this.id = id;
         this.titulo = titulo;
         this.autor = autor;
-        this.dataCadastro = dataCadastro;
         status = true;
+
+        // Método para converter a data do tipo LocalDate para Date
+        FormatarData formatarData = new FormatarData();
+        dataCadastro = formatarData.converterDataSql(data);
+        setDataAtualizacao(dataCadastro);
     }
-
-    private String livroInformacao(){
-
-        return "Id do livro: " + id + " Titulo: " + titulo
-                + " Autor: " + autor.getNome() + " Disponibilidade " + (status ? "Disponível" : "Indisponível")  + " Data de cadastro: " + dataCadastro
-                + " Data de atualização: " + dataAtualizacao;
-    }
-
 
     public String getTitulo() {
         return titulo;
     }
-
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
 
-    public Autor getAutor() {
+    public String getAutor() {
         return autor;
     }
-
-    public void setAutor(Autor autor) {
+    public void setAutor(String autor) {
         this.autor = autor;
     }
 
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
 
-    public LocalDate getDataAtualizacao() {
+    public Date getDataAtualizacao() {
         return dataAtualizacao;
     }
-
-    public void setDataAtualizacao(LocalDate dataAtualizacao) {
+    public void setDataAtualizacao(Date dataAtualizacao) {
         this.dataAtualizacao = dataAtualizacao;
     }
 
-    public LocalDate getDataCadastro(){
+    public Date getDataCadastro(){
        return dataCadastro;
     }
-
-    public void setDataCadastro(LocalDate dataCadastro){
+    public void setDataCadastro(Date dataCadastro){
         this.dataCadastro = dataCadastro;
     }
 
     public boolean getStatus() {
         return status;
     }
-
     public void setStatus(boolean status) {
         this.status = status;
+    }
+
+    private String dados(){
+        return "Id: " + id + " Titulo: " + titulo
+                + " Autor: " + autor + " Disponibilidade " + (status ? " Disponível" : " Indisponível")  + " Data de cadastro: " + dataCadastro.toString()
+                + " Data de atualização: " + dataAtualizacao.toString();
     }
 
 
