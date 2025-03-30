68cfd17ab599f68922c85704af64c9b7ded5a599
caiquepirs <c.pires@ba.estudante.senai.br>
2025-03-30 02:43:04 -0300

    Criado o método para cadastrar um novo Cliente na tabela Clientes no banco de dados Mysql

--- src/main/java/module/cliente/repository/ClienteRepository.java
+++ src/main/java/module/cliente/repository/ClienteRepository.java
@@ -1,4 +1,30 @@
 package module.cliente.repository;
+import module.cliente.model.Cliente;
+import java.sql.*;
+import static config.ConexaoMYSQL.getConexao;
 
 public class ClienteRepository {
+
+    public void criar(Cliente cliente){
+
+        try (Connection conexao = getConexao()) {
+            if (conexao != null) {
+                String sql = "INSERT INTO clientes (id, nome, email, cpf, dataNascimento) VALUES (?, ?, ?, ?, ?)";
+                PreparedStatement stmt = conexao.prepareStatement(sql);
+
+                stmt.setInt(1, cliente.getId());
+                stmt.setString(2, cliente.getNome());
+                stmt.setString(3, cliente.getEmail());
+                stmt.setString(4, cliente.getCpf());
+                stmt.setDate(5, cliente.getDataNascimento());
+                stmt.executeUpdate();
+                System.out.println("Cliente cadastrado com sucesso!");
+
+            }
+        } catch (SQLException e) {
+            System.err.println("Erro ao cadastrar o cliente: " + e.getMessage());
+        }
+    }
+
+
 }
--- src/main/java/module/cliente/service/ClienteService.java
+++ src/main/java/module/cliente/service/ClienteService.java
@@ -2,26 +2,20 @@ package module.cliente.service;
 
 import module.biblioteca.service.FormatarData;
 import module.cliente.model.Cliente;
+import module.cliente.repository.ClienteRepository;
 
 import java.time.LocalDate;
 
 public class ClienteService {
-    public void criar(int id, String nome,String email, String dataString){
+    private ClienteRepository repository = new ClienteRepository();
 
-        LocalDate dataNascimento;
-        Cliente cliente = null;
-        FormatarData formatarData = null;
-
-        dataNascimento = formatarData.formatar(dataString);
-
-        cliente.setId(id);
-        cliente.setNome(nome);
-        cliente.setEmail(email);
-        cliente.setDataNascimento(dataNascimento);
-
+    public void Cadastrar(int id, String nome, String email, String cpf, LocalDate data){
+        Cliente cliente = new Cliente(id, nome, email, cpf, data);
+        repository.criar(cliente);
     }
 
-    public void editar(){
+
+    public void atualizar(){
 
     }
 
