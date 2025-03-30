a49db05458a648b74e4f5f7b58efd3aec6b93555
caiquepirs <c.pires@ba.estudante.senai.br>
2025-03-30 02:23:38 -0300

    Atualizando a classe de modelo Cliente, adicionando novos atributos, método construtor e modificando o tipo de dado de LocalDate para date (para melhor performace no banco de dados)

--- src/main/java/module/cliente/model/Cliente.java
+++ src/main/java/module/cliente/model/Cliente.java
@@ -1,52 +1,43 @@
 package module.cliente.model;
 
-import module.biblioteca.model.Biblioteca;
-import module.biblioteca.service.FormatarData;
-
+import java.sql.Date;
 import java.time.LocalDate;
 
 public class Cliente {
-
-
     private int id;
     private String nome;
     private String email;
-    private LocalDate dataNascimento;
+    private String cpf;
+    private Date dataNascimento;
 
-    public String dadosCliente(){
-        return "Id: " + id + " Nome: " + nome
-                + " Email: " + email + " Data de nascimento: " + dataNascimento;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
+    public Cliente(int id, String nome, String email, String cpf, LocalDate data){
         this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
         this.nome = nome;
-    }
-
-    public LocalDate getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(LocalDate dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
         this.email = email;
+        this.cpf = cpf;
+
+        // Convertendo o tipo LocalDate para o tipo Datesql
+        Date sqldate = Date.valueOf(data);
+        setDataNascimento(sqldate);
+    }
+
+    public int getId() { return id;}
+    public void setId(int id) { this.id = id; }
+
+    public String getNome() { return nome;}
+    public void setNome(String nome) {this.nome = nome;}
+
+    public Date getDataNascimento() {return dataNascimento;}
+    public void setDataNascimento(Date dataNascimento) { this.dataNascimento = dataNascimento; }
+
+    public String getCpf() { return cpf; }
+    public void setCpf(String cpf) { this.cpf = cpf; }
+
+    public String getEmail() { return email;}
+    public void setEmail(String email) { this.email = email; }
+
+    public String dados(){
+        return "{Id: " + id + " Nome: " + nome
+                + "Cpf: " + cpf + " Email: " + email + " Data de nascimento: " + dataNascimento.toString() + "}";
     }
 }
