1569d446a665a5f47d3b273f18853dec49bb1063
caiquepirs <c.pires@ba.estudante.senai.br>
2025-03-30 10:47:50 -0300

    Criado o método para converter uma data do tipo LocalDate para Date do tipo sql e poder enviar o formato correto para o banco de dados;

--- src/main/java/module/biblioteca/service/FormatarData.java
+++ src/main/java/module/biblioteca/service/FormatarData.java
@@ -2,6 +2,7 @@ package module.biblioteca.service;
 
 import module.biblioteca.exception.ErroFormatarDataException;
 
+import java.sql.Date;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -9,16 +10,14 @@ public class FormatarData {
 
     private LocalDate dataFormatada;
 
-    public LocalDate formatar(String dataString) throws ErroFormatarDataException{
+    public LocalDate formatarLocalDate(String dataString) throws ErroFormatarDataException{
 
            try{
-
                // Formato padrão da data
                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
                // Converter a String para LocalDate
                dataFormatada = LocalDate.parse(dataString, formatter);
-
                return dataFormatada;
 
            }catch(ErroFormatarDataException e){
@@ -26,4 +25,16 @@ public class FormatarData {
            }
 
     }
+
+    public Date converterDataSql(LocalDate data){
+        try{
+            // Convertendo o tipo LocalDate para o tipo Datesql
+            Date sqldate = Date.valueOf(data);
+            return sqldate;
+
+        } catch (ErroFormatarDataException e){
+            throw new ErroFormatarDataException();
+        }
+
     }
+}
