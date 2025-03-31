5b4746b9c8ad674e17704f2216f7a7244eee55ed
caiquepirs <c.pires@ba.estudante.senai.br>
2025-03-31 05:16:19 -0300

    Criado o método que cadastra um novo empréstimo no banco de dados, além de alterar o status do livro para "Emprestado"

---
+++ src/main/java/module/emprestimo/repository/EmprestimoRepository.java
@@ -0,0 +1,49 @@
+package module.emprestimo.repository;
+
+import module.emprestimo.model.Emprestimo;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static config.ConexaoMYSQL.getConexao;
+
+public class EmprestimoRepository {
+
+    public void criar(Emprestimo emprestimo){
+        try(Connection conexao = getConexao()){
+            if(conexao != null){
+                String sql = "INSERT INTO emprestimos(id, dataEmprestimo, idCliente, idLivro, status) VALUES(?, ?, ?, ?, ?)";
+                PreparedStatement stmt = conexao.prepareStatement(sql);
+
+                // Cria o empréstimo no banco de dados
+                stmt.setInt(1, emprestimo.getId());
+                stmt.setDate(2, emprestimo.getDataEmprestimo());
+                stmt.setInt(3, emprestimo.getCliente().getId());
+                stmt.setInt(4, emprestimo.getLivro().getId());
+                stmt.setString(5, "Ativo");
+                stmt.executeUpdate();
+
+                // Altera o status do livro para indisponível ao ser emprestado
+                String sql1 = "UPDATE livros SET statusLivro = FALSE WHERE id = ? ";
+                PreparedStatement stmt1 = conexao.prepareStatement(sql1);
+                stmt1.setInt(1, emprestimo.getLivro().getId());
+                stmt1.executeUpdate();
+
+                System.out.println("Empréstimo do livro criado com sucesso!");
+            }
+
+        }catch (SQLException e){
+            System.out.println("Erro ao criar o empréstimo: " + e.getMessage());
+        }
+
+    }
+
+    public void pesquisar(){
+
+    }
+
+    public void excluir(){
+
+    }
+}
