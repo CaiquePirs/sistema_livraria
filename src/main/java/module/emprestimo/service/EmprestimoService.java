bf9ec93fddb56df4847d24f40e0345c7cb2043b6
caiquepirs <c.pires@ba.estudante.senai.br>
2025-03-31 05:20:13 -0300

    O método "criar" antes de solicitar o cadastro de um novo empréstimo no banco de dados, ele verifica se existe um cliente e um livro com o ID informado

---
+++ src/main/java/module/emprestimo/service/EmprestimoService.java
@@ -0,0 +1,43 @@
+package module.emprestimo.service;
+
+import module.cliente.model.Cliente;
+import module.cliente.service.ClienteService;
+import module.emprestimo.model.Emprestimo;
+import module.emprestimo.repository.EmprestimoRepository;
+import module.livro.model.Livro;
+import module.livro.service.LivroService;
+
+import java.sql.Date;
+
+public class EmprestimoService {
+    public void criar(int id, Date dataEmprestimo, int idCliente, int idLivro){
+
+        // Consulta o id do cliente no banco de dados e retorna o objeto Cliente
+        Cliente cliente;
+        ClienteService clienteService = new ClienteService();
+        cliente = clienteService.pesquisar(idCliente);
+
+        // Consulta o id do livro no banco de dados e retorna o objeto Livro
+        Livro livro;
+        LivroService livroService = new LivroService();
+        livro = livroService.pesquisar(idLivro);
+
+        // Realiza o empréstimo do livro
+        Emprestimo emprestimo = new Emprestimo(id, dataEmprestimo, cliente, livro);
+        EmprestimoRepository repository = new EmprestimoRepository();
+        repository.criar(emprestimo);
+    }
+
+    public void pesquisar(){
+
+    }
+
+    public void devolver(){
+
+    }
+
+    public void excluir(){
+
+    }
+
+}
