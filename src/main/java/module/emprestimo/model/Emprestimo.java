7ba06f284d696f85f62e7501b4f4796a650ba75d
caiquepirs <c.pires@ba.estudante.senai.br>
2025-03-31 05:13:43 -0300

    Adicionando e modificando os atributos do objeto Livro, criando os getters, setters e o método "dados" que retorna os dados do empréstimo

--- src/main/java/module/emprestimo/model/Emprestimo.java
+++ src/main/java/module/emprestimo/model/Emprestimo.java
@@ -3,31 +3,42 @@ package module.emprestimo.model;
 import module.cliente.model.Cliente;
 import module.livro.model.Livro;
 
-import java.time.LocalDate;
+import java.sql.Date;
 
 public class Emprestimo {
-
     private int id;
-    private LocalDate dataEmprestimo;
+    private Date dataEmprestimo;
     private Livro livro;
     private Cliente cliente;
+    private String status;
 
-    Emprestimo(int id, LocalDate dataEmprestimo, Cliente cliente, Livro livro){
-
-        this.dataEmprestimo = dataEmprestimo;
+    public Emprestimo(int id, Date dataEmprestimo, Cliente cliente, Livro livro){
         this.id = id;
+        this.dataEmprestimo = dataEmprestimo;
         this.cliente = cliente;
         this.livro = livro;
-
-        // Marca o livro como 'Indisponivel'
-        livro.setStatus(false);
-
     }
 
-    public String emprestimoInformacao(){
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
-        return " Id:" + id + " Nome do cliente: " + cliente.getNome()
-                + " Livro: " + livro.getTitulo() + " Autor: " + livro.getAutor() + " Data do emprestimo: " + dataEmprestimo;
+    public Cliente getCliente() { return cliente; }
+    public void setCliente(Cliente cliente) { this.cliente = cliente; }
+
+    public Livro getLivro() { return livro; }
+    public void setLivro(Livro livro) { this.livro = livro; }
+
+    public Date getDataEmprestimo() { return dataEmprestimo; }
+    public void setDataEmprestimo(Date dataEmprestimo) { this.dataEmprestimo = dataEmprestimo; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+
+    public String dados(){
+        return "Id: " + id + " Nome do cliente: " + cliente.getNome() + " Cpf do cliente: " + cliente.getCpf()
+                + " Email: " + cliente.getEmail()
+                + " Livro: " + livro.getTitulo() + " Data do empréstimo: " + dataEmprestimo.toString()
+                + " Status: " + getStatus();
     }
 
 }
