b7ab8d2d1dd29c529102b943f9f92a1d4475519c
caiquepirs <c.pires@ba.estudante.senai.br>
2025-03-30 22:12:59 -0300

    Removendo o objeto Autor, agora o autor é definido na criação do livro

--- src/main/java/module/biblioteca/model/Biblioteca.java
+++ src/main/java/module/biblioteca/model/Biblioteca.java
@@ -2,7 +2,6 @@ package module.biblioteca.model;
 
 import module.cliente.model.Cliente;
 import module.emprestimo.model.Emprestimo;
-import module.livro.model.Autor;
 import module.livro.model.Livro;
 
 import java.util.ArrayList;
@@ -10,7 +9,6 @@ import java.util.List;
 
 public class Biblioteca {
     List<Livro> livros = new ArrayList<>();
-    List<Autor> autores = new ArrayList<>();
     List<Cliente> clientes = new ArrayList<>();
     List<Emprestimo> emprestimos = new ArrayList<>();
 }
