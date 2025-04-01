de0f950c0a079f184ff997e46121cc2899803ca0
caiquepirs <c.pires@ba.estudante.senai.br>
2025-04-01 05:00:07 -0300

    Criado o método que pesquisa o empréstimo de um livro existente no banco de dados e retorna as informações do empréstimo, do livro e do cliente

--- src/main/java/module/emprestimo/repository/EmprestimoRepository.java
+++ src/main/java/module/emprestimo/repository/EmprestimoRepository.java
@@ -1,10 +1,12 @@
 package module.emprestimo.repository;
 
+import module.cliente.model.Cliente;
+import module.cliente.service.ClienteService;
 import module.emprestimo.model.Emprestimo;
+import module.livro.model.Livro;
+import module.livro.service.LivroService;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 import static config.ConexaoMYSQL.getConexao;
 
@@ -39,8 +41,44 @@ public class EmprestimoRepository {
 
     }
 
-    public void pesquisar(){
+    public void pesquisar(int id){
+        try(Connection conexao = getConexao()){
+            if(conexao != null){
+                String sql = "SELECT * FROM emprestimos WHERE id = ?";
+                PreparedStatement stmt = conexao.prepareCall(sql);
+                stmt.setInt(1, id);
 
+                ResultSet rs = stmt.executeQuery();
+                if(rs.next()){
+                    int id_emprestimo = rs.getInt("id");
+                    int id_livro = rs.getInt("idLivro");
+                    int id_cliente = rs.getInt("idCliente");
+                    Date data_emprestimo = rs.getDate("dataEmprestimo");
+                    String status = rs.getString("status");
+
+                    // Pesquisa o cliente no banco de dados através do id_cliente e retorna os dados do Cliente
+                    Cliente cliente = null;
+                    ClienteService service = new ClienteService();
+                    cliente = service.pesquisar(id_cliente);
+
+                    // Pesquisa o livro no banco de dados através do id_livro e retorna os dados do livro
+                    Livro livro = null;
+                    LivroService livroService = new LivroService();
+                    livro = livroService.pesquisar(id_livro);
+
+                    Emprestimo emprestimo = new Emprestimo(id_emprestimo, data_emprestimo, cliente, livro);
+                    emprestimo.setStatus(status);
+                    System.out.println(emprestimo.dados());
+
+                } else {
+                    System.out.println("Id do empréstimo não encontrado");
+                }
+
+            }
+
+        }catch(SQLException e){
+            System.out.println("Erro ao consultar o empréstimo: " + e.getMessage());
+        }
     }
 
     public void devolver(){
