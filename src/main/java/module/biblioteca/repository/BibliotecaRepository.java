9d94e243324f76c4492c406ebfe7e65c44d1628b
caiquepirs <c.pires@ba.estudante.senai.br>
2025-04-04 06:43:22 -0300

    Criado o método que busca todos os empréstimos cadastrados na biblioteca.

--- src/main/java/module/biblioteca/repository/BibliotecaRepository.java
+++ src/main/java/module/biblioteca/repository/BibliotecaRepository.java
@@ -1,6 +1,9 @@
 package module.biblioteca.repository;
 import module.cliente.model.Cliente;
+import module.cliente.service.ClienteService;
+import module.emprestimo.model.Emprestimo;
 import module.livro.model.Livro;
+import module.livro.service.LivroService;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -63,7 +66,44 @@ public class BibliotecaRepository {
         return livros;
     }
 
-    public void listarEmprestimos(){
+    public List<Emprestimo> buscarEmprestimos(){
+        List<Emprestimo> emprestimos = new ArrayList<>();
 
+        try(Connection conexao = getConexao()){
+            if(conexao != null){
+                String sql = "SELECT * FROM emprestimos ORDER BY id ASC";
+                Statement stmt = conexao.createStatement();
+
+                ResultSet rs = stmt.executeQuery(sql);
+                while(rs.next()) {
+                 int id_emprestimo = rs.getInt("id");
+                 int id_cliente = rs.getInt("idCliente");
+                 int id_livro = rs.getInt("idLivro");
+                 Date data_emprestimo = rs.getDate("dataEmprestimo");
+                 Date data_devolucao = rs.getDate("dataDevolucao");
+                 String status = rs.getString("status");
+
+                 // Pesquisa o cliente pelo id_cliente e retorna os dados do cliente que realizou o empréstimo
+                 ClienteService clienteService = new ClienteService();
+                 Cliente cliente = clienteService.pesquisar(id_cliente);
+
+                 // Pesquisa o livro pelo id_livro e retorna os dados do Livro empréstado
+                 LivroService livroService = new LivroService();
+                 Livro livro = livroService.pesquisar(id_livro);
+
+                 // Cria um objeto do tipo 'Empréstimo' com as informações obtidas
+                 Emprestimo emprestimo = new Emprestimo(id_emprestimo, data_emprestimo, cliente, livro);
+                 emprestimo.setDataDevolucao(data_devolucao);
+                 emprestimo.setStatus(status);
+
+                 // Adiciona cada empréstimo cadastrado na lista de empréstimos
+                 emprestimos.add(emprestimo);
+                }
+            }
+
+        } catch (SQLException e){
+            System.out.println("Erro ao listar os empréstimos: " + e.getMessage());
+        }
+        return emprestimos;
     }
 }
