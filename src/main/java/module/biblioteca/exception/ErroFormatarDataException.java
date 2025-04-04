201d314ae590775ba0e529d3768c98ac5bf5a157
caiquepirs <c.pires@ba.estudante.senai.br>
2025-04-04 04:13:31 -0300

    Excluindo a classe Biblioteca, FormatarData e ErroFormatarDataException

--- src/main/java/module/biblioteca/exception/ErroFormatarDataException.java
+++
@@ -1,7 +0,0 @@
-package module.biblioteca.exception;
-
-public class ErroFormatarDataException extends RuntimeException{
-    public String ErroFormatarDataException(){
-        return "Erro ao formatar a data";
-    }
-}
